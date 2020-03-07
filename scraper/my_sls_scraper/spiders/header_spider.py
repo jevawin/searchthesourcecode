d0e5876f0c5508cb5982237fafa898f7a77d7380
Jamie Evawin <jamie.evawin@musicmagpie.co.uk>
2020-03-07 16:39:36 +0000

    Python Scrapy, first draft

---
+++ scraper/my_sls_scraper/spiders/header_spider.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+
+
+def netball_team(body):
+    query = "DOCTYPE"
+    result = body.find(query)
+    return result
+
+
+class HeaderSpider(scrapy.Spider):
+    name = 'header_spider'
+    start_urls = ['https://www.hillarys.co.uk']
+    allowed_domains = ['hillarys.co.uk']
+
+    def parse(self, response):
+        url = response.url
+        body = response.text
+        matches = netball_team(body)
+
+        # yield {'url': response.url, 'response': response.text}
+        yield {
+            'url': url,
+            'matches': matches
+        }
+
+        trending_links = LxmlLinkExtractor(
+            allow=r'^http', deny=r'\?', unique=True).extract_links(response)
+
+        for next_page in trending_links:
+            yield response.follow(next_page, self.parse)
