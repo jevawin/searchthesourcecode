58ad7aac6f3e94d4d7903c08dbdfbaa5565a93cc
Jamie Evawin <jamie.evawin@musicmagpie.co.uk>
2020-03-13 15:52:59 +0000

    🔧Echoing to console when running spider.

--- clear_run.sh
+++ clear_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clear && true > searchthesourcecode/feed/searchthesourcecode.json && cd searchthesourcecode && scrapy crawl searchthesourcecode --output "feed/%(name)s.json" --output-format json -a query=$1 -a start=$2
+clear && echo "Running spider..." && true > searchthesourcecode/feed/searchthesourcecode.json && cd searchthesourcecode && scrapy crawl searchthesourcecode --output "feed/%(name)s.json" --output-format json -a query=$1 -a start=$2
