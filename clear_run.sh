6921982ba247b1be5977a4c7b7100ba445b10cf7
Jamie Evawin <jamie.evawin@musicmagpie.co.uk>
2020-03-13 16:27:57 +0000

    🔧Adding logs to mkdir.

--- clear_run.sh
+++ clear_run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-clear && echo "Running spider..." && mkdir -p searchthesourcecode/feed && true > searchthesourcecode/feed/searchthesourcecode.json && cd searchthesourcecode && scrapy crawl searchthesourcecode --output "feed/%(name)s.json" --output-format json -a query=$1 -a start=$2
+clear
+echo "Running spider..."
+mkdir -p searchthesourcecode/feed
+mkdir -p searchthesourcecode/logs
+true > searchthesourcecode/feed/searchthesourcecode.json
+cd searchthesourcecode
+scrapy crawl searchthesourcecode --output "feed/%(name)s.json" --output-format json -a query=$1 -a start=$2
